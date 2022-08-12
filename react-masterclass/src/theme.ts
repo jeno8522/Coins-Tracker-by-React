04a6a2c75bb9749f88a4799a6e62252f4c81338f
jeno8522 <68889383+jeno8522@users.noreply.github.com>
2022-08-13 00:58:22 +0900

    finished coins tracking by react

---
+++ react-masterclass/src/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+const lightTheme: DefaultTheme = {
+  bgColor: "whitesmoke",
+  textColor: "whitesmoke",
+  accentColor: "#9c88ff",
+  coinsColor: "#6E7EAA",
+};
+
+const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+  coinsColor: "whitesmoke",
+};
+
+export { lightTheme, darkTheme };
